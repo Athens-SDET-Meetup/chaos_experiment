047c3ab19a6368676605ac6a176adf196f87eb46
Giannis Papadakis <gpapadakis@gwi.com>
2024-02-19 16:24:52 +0200

    final fix

--- consumer/main.go
+++ consumer/main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,30 +12,18 @@ func main() {
 	r := gin.New()
 
 	r.GET("/consuming", func(ctx *gin.Context) {
-		retryTimes := 0
-
-		for i := 1; i < 5; i++ {
-
-			response, err := http.Get(fmt.Sprintf("http://%s:%s/", os.Getenv("PROVIDER_URL"), os.Getenv("PROVIDER_PORT")))
-			if err != nil {
-				fmt.Printf("error making http request: %s\n", err)
-				os.Exit(1)
-			}
-
-			if response.StatusCode == 200 {
-				break
-			} else if response.StatusCode == 500 {
-				retryTimes += 1
-			}
-
-			time.Sleep(2 * time.Second)
-
+		response, err := http.Get(fmt.Sprintf("http://%s:%s/", os.Getenv("PROVIDER_URL"), os.Getenv("PROVIDER_PORT")))
+		if err != nil {
+			fmt.Printf("error making http request: %s\n", err)
+			os.Exit(1)
+		} else {
+			fmt.Printf("HTTP request successful: %s\n", err)
 		}
 
-		if retryTimes >= 4 {
-			ctx.String(500, "Internal server error")
-		} else {
+		if response.StatusCode == 200 {
 			ctx.String(200, "Successfully consuming message")
+		} else {
+			ctx.String(500, "Internal server error")
 		}
 
 	})
